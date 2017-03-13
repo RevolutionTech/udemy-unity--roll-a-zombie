cad57b94aaca81ff0dad92083b2c9d54cb56994b
Lucas Connors <lucas.revolutiontech@gmail.com>
2017-03-12 22:34:29 -0700

    Allow user to select and push zombies

---
+++ Assets/GameManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	private int selectedZombiePosition = 0;
+	public GameObject selectedZombie;
+	public List<GameObject> zombies;
+	public Vector3 selectedSize;
+	public Vector3 defaultSize;
+
+	// Use this for initialization
+	void Start () {
+		SelectZombie (selectedZombie);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("left")) {
+			GetZombieLeft ();
+		}
+		if (Input.GetKeyDown ("right")) {
+			GetZombieRight ();
+		}
+		if (Input.GetKeyDown ("up")) {
+			PushUp ();
+		}
+	}
+
+	void GetZombieLeft() {
+		if (selectedZombiePosition == 0) {
+			selectedZombiePosition = 3;
+		} else {
+			selectedZombiePosition -= 1;
+		}
+		SelectZombie (zombies [selectedZombiePosition]);
+	}
+
+	void GetZombieRight() {
+		if (selectedZombiePosition == 3) {
+			selectedZombiePosition = 0;
+		} else {
+			selectedZombiePosition += 1;
+		}
+		SelectZombie(zombies [selectedZombiePosition]);
+	}
+
+	void SelectZombie(GameObject newZombie) {
+		selectedZombie.transform.localScale = defaultSize;
+		selectedZombie = newZombie;
+		newZombie.transform.localScale = selectedSize;
+	}
+
+	void PushUp() {
+		Rigidbody rb = selectedZombie.GetComponent<Rigidbody> ();
+		rb.AddForce (0, 0, 10, ForceMode.Impulse);
+	}
+}
